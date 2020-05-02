a1dd651c43b1384fbf92eccf6d8a0da645eae1b7
AgusIwanPurnama17710009 <64255422+agusiwan@users.noreply.github.com>
2020-05-02 16:59:39 +0800

    Penambahan HitungActivity, panggil dengan instant

--- app/src/main/java/com/raktikumandroid/MainActivity.kt
+++ app/src/main/java/com/raktikumandroid/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.raktikumandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView1 = findViewById<TextView>(R.id.textView1)
+        val textView1 = findViewById<TextView>(R.id.Panjang)
+        val button1 =  findViewById<Button>(R.id.button1)
+
 
         textView1.setOnClickListener {
 
@@ -21,6 +23,10 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        button1.setOnClickListener{
+            val intent  =   Intent(this@MainActivity,HitungActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
