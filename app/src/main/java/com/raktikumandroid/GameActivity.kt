469ae7a1a82ff7d0d7bad76ed611ba90dde14de0
AgusIwanPurnama17710009 <64255422+agusiwan@users.noreply.github.com>
2020-05-16 16:17:27 +0800

    Membuat Insert Extra & AlertDialog with EditText

--- app/src/main/java/com/raktikumandroid/GameActivity.kt
+++ app/src/main/java/com/raktikumandroid/GameActivity.kt
@@ -1,12 +1,95 @@
 package com.raktikumandroid
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_game.*
+import kotlin.random.Random
+
+private const val BATU = 1
+private const val GUNTING = 2
+private const val KERTAS = 3
 
 class GameActivity : AppCompatActivity() {
 
+    companion object{
+        const val EXTRA_USER = "username"
+    }
+
+    var username:String=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        username = intent.getStringExtra(EXTRA_USER)
+
+        var PilihanKomputer:Int
+        var pilihanPlayer:Int
+
+        buttonBatu.setOnClickListener{
+            textPlayer.text = "BATU"
+            pilihanPlayer = BATU
+            PilihanKomputer = getPilihanKomputer()
+            textKomputer.text = ""
+                textHasil.text = suwit(pilihanPlayer,pilihanKomputer = 1)
+                showPilihanKomputer(getPilihanKomputer()
+            )
+
+        }
+
+        buttonGunting.setOnClickListener{
+            textPlayer.text = "GUNTING"
+            pilihanPlayer = GUNTING
+            PilihanKomputer = getPilihanKomputer()
+            textKomputer.text = ""
+                textHasil.text = suwit(pilihanPlayer,pilihanKomputer = 2)
+                showPilihanKomputer(getPilihanKomputer()
+            )
+
+        }
+
+        buttonKertas.setOnClickListener{
+            textPlayer.text = "KERTAS"
+            pilihanPlayer = KERTAS
+            PilihanKomputer = getPilihanKomputer()
+            textKomputer.text = ""
+                textHasil.text = suwit(pilihanPlayer, pilihanKomputer = 3)
+                showPilihanKomputer(getPilihanKomputer()
+            )
+        }
+
+
+    }
+
+    fun getPilihanKomputer():Int{
+        return Random.nextInt(+ 3) + 1;
+    }
+    fun suwit (pilihanPlayer: Int, pilihanKomputer: Int):String{
+        var hasil: String = ""
+        if (pilihanPlayer == pilihanKomputer){
+            hasil = "SERI"
+            textHasil.setTextColor(Color.GRAY)
+        }
+        else if((pilihanPlayer == BATU && pilihanKomputer == KERTAS) ||
+            (pilihanPlayer == GUNTING && pilihanKomputer == BATU) ||
+            (pilihanPlayer == KERTAS && pilihanKomputer == GUNTING)){
+            hasil = username +" LOSE"
+            textHasil.setTextColor(Color.RED)
+        }
+        else{
+            hasil =  username +" WIN"
+            textHasil.setTextColor(Color.GREEN)
+        }
+
+        return hasil
+    }
+
+    fun showPilihanKomputer(pilihanKomputer: Int){
+       when (pilihanKomputer){
+           1 -> textKomputer.text = "Komputer memilih: BATU"
+           2 -> textKomputer.text = "Komputer memilih: GUNTING"
+           3 -> textKomputer.text = "Komputer memilih: KERTAS"
+       }
+
     }
 }
--- app/src/main/java/com/raktikumandroid/MainActivity.kt
+++ app/src/main/java/com/raktikumandroid/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import android.app.AlertDialog
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,6 +29,29 @@ class MainActivity : AppCompatActivity() {
         button1.setOnClickListener{
             val intent  =   Intent(this@MainActivity,HitungActivity::class.java)
             startActivity(intent)
+        }
+
+        buttonGunting.setOnClickListener{
+            var alerts : AlertDialog.Builder = AlertDialog.Builder(this)
+            val editUser = EditText(this)
+            alerts.setTitle("Username")
+            alerts.setMessage("Hai")
+            alerts.setView(editUser)
+
+            alerts.setPositiveButton("Yes"){dialog, which ->
+                var username = editUser.text.toString()
+                val intent  =   Intent(this@MainActivity,GameActivity::class.java)
+                intent.putExtra(GameActivity.EXTRA_USER, username)
+                startActivity(intent)
+
+            }
+
+            alerts.setNegativeButton("No"){dialog, which ->
+
+            }
+
+            alerts.show()
+
         }
     }
 
